431fce95288a315c9d636b3f2424ea45751d83b4
Carlos-SD <carlosfelipe088@outlook.es>
2023-09-07 12:26:05 -0500

    Primer menu

--- Integradora-Apo II/src/model/Main.java
+++ Integradora-Apo II/src/model/Main.java
@@ -1,5 +1,26 @@
 package model;
 
-public class Main{
-    
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("1. Nueva partida");
+        System.out.println("2. Ver puntaje");
+        System.out.println("3. Salir");
+        System.out.println("----------------------------------");
+
+        int opcion = Integer.parseInt(scanner.nextLine());
+        switch (opcion) {
+            case 1:
+                String nickname = scanner.nextLine();
+
+            case 2:
+
+            case 3:
+        }
+    }
 }
