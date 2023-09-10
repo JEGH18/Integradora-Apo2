fe1be2d7ac85482497cbd38d8280a7e6eb4abb7e
unknown <09juanesteban@gmail.com>
2023-09-10 11:15:13 -0500

    Se cambio el tablero de matrices a listas enlazadas

--- Integradora-Apo II/src/iu/Tablero.java
+++ Integradora-Apo II/src/iu/Tablero.java
@@ -1,33 +1,37 @@
 package iu;
 
+import java.util.LinkedList;
+
 public class Tablero {
-    private char[][] tablero;
+    private LinkedList<LinkedList<Character>> tablero;
 
     public Tablero() {
-        this.tablero = new char[8][8];
+        this.tablero = new LinkedList<>();
         // Inicializar el tablero con valores por defecto
         for (int i = 0; i < 8; i++) {
+            LinkedList<Character> fila = new LinkedList<>();
             for (int j = 0; j < 8; j++) {
-                this.tablero[i][j] = 'X';
+                fila.add('X');
             }
+            this.tablero.add(fila);
         }
         // Colocar la fuente (F) y el drenaje (D) en posiciones aleatorias
         int filaF = (int) (Math.random() * 8);
         int columnaF = (int) (Math.random() * 8);
-        this.tablero[filaF][columnaF] = 'F';
+        this.tablero.get(filaF).set(columnaF, 'F');
         int filaD = (int) (Math.random() * 8);
         int columnaD = (int) (Math.random() * 8);
         while (filaD == filaF && columnaD == columnaF) {
             filaD = (int) (Math.random() * 8);
             columnaD = (int) (Math.random() * 8);
         }
-        this.tablero[filaD][columnaD] = 'D';
+        this.tablero.get(filaD).set(columnaD, 'D');
     }
 
     public void modificar(int fila, int columna, char valor) {
         // Verificar que la posición sea válida y no sea la fuente o el drenaje
-        if (fila >= 0 && fila < 8 && columna >= 0 && columna < 8 && this.tablero[fila][columna] != 'F' && this.tablero[fila][columna] != 'D') {
-            this.tablero[fila][columna] = valor;
+        if (fila >= 0 && fila < 8 && columna >= 0 && columna < 8 && this.tablero.get(fila).get(columna) != 'F' && this.tablero.get(fila).get(columna) != 'D') {
+            this.tablero.get(fila).set(columna, valor);
         }
     }
 
@@ -37,12 +41,13 @@ public class Tablero {
     }
 
     public void mostrar() {
-        for (int i = 0; i < 8; i++) {
-            for (int j = 0; j < 8; j++) {
-                System.out.print(this.tablero[i][j] + " ");
+        for (LinkedList<Character> fila : this.tablero) {
+            for (char c : fila) {
+                System.out.print(c + " ");
             }
             System.out.println();
         }
     }
 }
+
 
